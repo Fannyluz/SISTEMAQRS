279f42e424f4e4c2520248a1e25f34b8f678acac
Fannyluz <30957135+Fannyluz@users.noreply.github.com>
2020-11-29 21:09:31 -0500

    plantillas

---
+++ vistas/inc/NavLateral.php
@@ -0,0 +1,152 @@
+<section class="full-box nav-lateral">
+			<div class="full-box nav-lateral-bg show-nav-lateral"></div>
+			<div class="full-box nav-lateral-content">
+				<figure class="full-box nav-lateral-avatar">
+					<i class="far fa-times-circle show-nav-lateral"></i>
+					<img src="<?php echo SERVERURL; ?>vistas/assets/avatar/Avatar.png" class="img-fluid" alt="Avatar">
+					<figcaption class="roboto-medium text-center">
+						Carlos Alfaro <br><small class="roboto-condensed-light">Administrador</small>
+					</figcaption>
+				</figure>
+				<div class="full-box nav-lateral-bar"></div>
+				<nav class="full-box nav-lateral-menu">
+					<ul>
+						<li>
+							<a href="home.html"><i class="fab fa-dashcube fa-fw"></i> &nbsp; Inicio</a>
+                        </li>
+                        
+						<li>
+							<a href="company.html"><i class="fas fa-store-alt fa-fw"></i> &nbsp; Empresa</a>
+						</li>
+						<li>
+							<a href="#" class="nav-btn-submenu"><i class="fas fa-book fa-fw"></i> &nbsp; Casos <i class="fas fa-chevron-down"></i></a>
+							<ul>
+								<li>
+									<a href="client-new.html"><i class="fas fa-plus fa-fw"></i> &nbsp; Agregar Caso </a>
+								</li>
+								<li>
+									<a href="client-list.html"><i class="fas fa-clipboard-list fa-fw"></i> &nbsp; Listar Casos</a>
+								</li>
+								<li>
+									<a href="client-search.html"><i class="fas fa-search fa-fw"></i> &nbsp; Buscar Caso</a>
+								</li>
+							</ul>
+                        </li>
+                        <li>
+							<a href="#" class="nav-btn-submenu"><i class="fas fa-pallet fa-fw"></i> &nbsp; Tipo QRS <i class="fas fa-chevron-down"></i></a>
+							<ul>
+								<li>
+									<a href="client-new.html"><i class="fas fa-plus fa-fw"></i> &nbsp; Agregar Tipo QRS </a>
+								</li>
+								<li>
+									<a href="client-list.html"><i class="fas fa-clipboard-list fa-fw"></i> &nbsp; Listar Tipo QRS</a>
+								</li>
+								<li>
+									<a href="client-search.html"><i class="fas fa-search fa-fw"></i> &nbsp; Buscar Tipo QRS</a>
+								</li>
+							</ul>
+                        </li>
+                        <li>
+							<a href="#" class="nav-btn-submenu"><i class="fas fa-users fa-fw"></i> &nbsp; Tipo Usuario <i class="fas fa-chevron-down"></i></a>
+							<ul>
+								<li>
+									<a href="client-new.html"><i class="fas fa-user-plus fa-fw"></i> &nbsp; Agregar Tipo Usuario </a>
+								</li>
+								<li>
+									<a href="client-list.html"><i class="fas fa-clipboard-list fa-fw"></i> &nbsp; Listar Tipo Usuarios</a>
+								</li>
+								<li>
+									<a href="client-search.html"><i class="fas fa-search fa-fw"></i> &nbsp; Buscar Tipo Usuario</a>
+								</li>
+							</ul>
+                        </li>
+                        <li>
+							<a href="#" class="nav-btn-submenu"><i class="fas fa-users fa-fw"></i> &nbsp; Tipo Personal UptVirtual<i class="fas fa-chevron-down"></i></a>
+							<ul>
+								<li>
+									<a href="client-new.html"><i class="fas fa-user-plus fa-fw"></i> &nbsp; Agregar Tipo Personal UptVirtual </a>
+								</li>
+								<li>
+									<a href="client-list.html"><i class="fas fa-clipboard-list fa-fw"></i> &nbsp; Listar Tipo Personal UptVirtual</a>
+								</li>
+								<li>
+									<a href="client-search.html"><i class="fas fa-search fa-fw"></i> &nbsp; Buscar Tipo Personal UptVirtual</a>
+								</li>
+							</ul>
+						</li>
+                        <li>
+							<a href="#" class="nav-btn-submenu"><i class="fas fa-user fa-fw"></i> &nbsp; Personal UptVirtual<i class="fas fa-chevron-down"></i></a>
+							<ul>
+								<li>
+									<a href="client-new.html"><i class="fas fa-user-plus fa-fw"></i> &nbsp; Agregar Personal UptVirtual</a>
+								</li>
+								<li>
+									<a href="client-list.html"><i class="fas fa-clipboard-list fa-fw"></i> &nbsp; Listar Personal UptVirtual</a>
+								</li>
+								<li>
+									<a href="client-search.html"><i class="fas fa-search fa-fw"></i> &nbsp; Buscar Personal UptVirtual</a>
+								</li>
+							</ul>
+                        </li>
+                        <li>
+							<a href="#" class="nav-btn-submenu"><i  class="fas  fa-user fa-fw"></i> &nbsp; Usuario UptVirtual<i class="fas fa-chevron-down"></i></a>
+							<ul>
+								<li>
+									<a href="client-new.html"><i class="fas fa-user-plus fa-fw"></i> &nbsp; Agregar Usuario UptVirtual</a>
+								</li>
+								<li>
+									<a href="client-list.html"><i class="fas fa-clipboard-list fa-fw"></i> &nbsp; Listar Usuario UptVirtual</a>
+								</li>
+								<li>
+									<a href="client-search.html"><i class="fas fa-search fa-fw"></i> &nbsp; Buscar Usuario UptVirtual</a>
+								</li>
+							</ul>
+                        </li>
+                        <li>
+							<a href="#" class="nav-btn-submenu"><i class="fas fa-pallet fa-fw"></i> &nbsp; Actividades Pendientes <i class="fas fa-chevron-down"></i></a>
+							<ul>
+								<li>
+									<a href="item-new.html"><i class="fas fa-clipboard-list fa-fw"></i> &nbsp; Listar Actividades</a>
+								</li>
+								<li>
+									<a href="item-list.html"><i class="fas fa-share fa-fw"></i> &nbsp; Asignar Actividad</a>
+								</li>
+								
+                            </ul>
+                        </li>
+                        <li>
+							<a href="#" class="nav-btn-submenu"><i class="fas fa-pallet fa-fw"></i> &nbsp; Actividades Pendientes <i class="fas fa-chevron-down"></i></a>
+							<ul>
+								<li>
+									<a href="item-new.html"><i class="fas fa-clipboard-list fa-fw"></i> &nbsp; Listar Actividades</a>
+								</li>
+								<li>
+									<a href="item-list.html"><i class="fas fa-share fa-fw"></i> &nbsp; Asignar Actividad</a>
+								</li>
+								
+							</ul>
+						</li>
+						<li>
+							<a href="#" class="nav-btn-submenu"><i class="fas fa-database fa-fw"></i> &nbsp; Reportes <i class="fas fa-chevron-down"></i></a>
+							<ul>
+								<li>
+									<a href="reservation-new.html"><i class="fas fa-plus fa-fw"></i> &nbsp; Nuevo préstamo</a>
+								</li>
+								<li>
+									<a href="reservation-reservation.html"><i class="far fa-calendar-alt fa-fw"></i> &nbsp; Reservaciones</a>
+								</li>
+								<li>
+									<a href="reservation-pending.html"><i class="fas fa-hand-holding-usd fa-fw"></i> &nbsp; Préstamos</a>
+								</li>
+								<li>
+									<a href="reservation-list.html"><i class="fas fa-clipboard-list fa-fw"></i> &nbsp; Finalizados</a>
+								</li>
+								<li>
+									<a href="reservation-search.html"><i class="fas fa-search-dollar fa-fw"></i> &nbsp; Buscar por fecha</a>
+								</li>
+							</ul>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</section>
