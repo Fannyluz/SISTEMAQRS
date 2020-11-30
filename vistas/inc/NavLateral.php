3684650f55d279c73d313637b2540709862325d0
Fannyluz <30957135+Fannyluz@users.noreply.github.com>
2020-11-30 09:35:47 -0500

    NOMBRES Y APELLIDOS

--- vistas/inc/NavLateral.php
+++ vistas/inc/NavLateral.php
@@ -5,7 +5,7 @@
 					<i class="far fa-times-circle show-nav-lateral"></i>
 					<img src="<?php echo SERVERURL; ?>vistas/assets/avatar/Avatar.png" class="img-fluid" alt="Avatar">
 					<figcaption class="roboto-medium text-center">
-						Carlos Alfaro <br><small class="roboto-condensed-light">Administrador</small>
+						NOMBRES Y APELLIDOS <br><small class="roboto-condensed-light">Administrador</small>
 					</figcaption>
 				</figure>
 				<div class="full-box nav-lateral-bar"></div>
