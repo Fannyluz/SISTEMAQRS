d1de5125cbcf55aa790401795ed21bbceaf6b45f
Fannyluz <30957135+Fannyluz@users.noreply.github.com>
2020-11-29 20:40:52 -0500

    otro

---
+++ config/APP.php
@@ -0,0 +1,11 @@
+<?php
+    const SERVERURL="http://localhost:8080/SISTEMAQRS/";
+    
+    const COMPANY="SISTEMAQRS";
+    const MONEDA ="$";
+
+    date_default_timezone_set("America/El_Salvador");
+    
+
+
+
---
+++ config/SERVER.php
@@ -0,0 +1,12 @@
+<?php
+
+const SERVER="localhost:8080";
+const DB="bd_sistemasqrs";
+const USER="root";
+const PASS="rootfanny";
+
+const SGBD="mysql:host=".SERVER.";dbname=".DB;
+
+const METHOD="AES-256-CBC";
+const SECRET_KEY='$SISTEMAQRS@2020';
+const SECRET_IV='654321';
