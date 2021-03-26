1035c2bec50929388ab918f5b51884802956cdc3
Fannyluz <30957135+Fannyluz@users.noreply.github.com>
2021-03-25 20:34:41 -0500

    xd

--- vistas/contenidos/listar-actividadAtendidasAll-view.php
+++ vistas/contenidos/listar-actividadAtendidasAll-view.php
@@ -35,10 +35,11 @@ if($_SESSION['privilegio_spm']!=1 && $_SESSION['privilegio_spm']!=2){
 
         </button>
       </input>
+        <!--
       <a href="../vistas/pdf/pdfAtendidasAll.php" type="submit" class="btn btn-" style="background-color:#10226a;color:white;">
         Exportar PDF 
 
-        </a>
+        </a>-->
     </form> 
     <p align="left">
     <form method="post" action="<?php echo SERVERURL; ?>ajax/wordAjax.php">
