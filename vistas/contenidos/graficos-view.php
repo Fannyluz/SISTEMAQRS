67d4c864fadf6529918c64fa78738094845d2d51
Fannyluz <30957135+Fannyluz@users.noreply.github.com>
2021-04-04 17:39:06 -0500

    donus

--- vistas/contenidos/graficos-view.php
+++ vistas/contenidos/graficos-view.php
@@ -58,6 +58,13 @@
 <script src="<?php echo SERVERURL; ?>vistas/Graficas/code/modules/export-data.js"></script>
 <script src="<?php echo SERVERURL; ?>vistas/Graficas/code/modules/accessibility.js"></script>
 
+<!-- DONUT-->
+<script src="<?php echo SERVERURL; ?>vistas/Graficas/code/highcharts.js"></script>
+<script src="<?php echo SERVERURL; ?>vistas/Graficas/code/highcharts-3d.js"></script>
+<script src="<?php echo SERVERURL; ?>vistas/Graficas/code/modules/exporting.js"></script>
+<script src="<?php echo SERVERURL; ?>vistas/Graficas/code/modules/export-data.js"></script>
+<script src="<?php echo SERVERURL; ?>vistas/Graficas/code/modules/accessibility.js"></script>
+
 <!-- COLUMNAS COMPARATIVAS -->
 <script src="<?php echo SERVERURL; ?>vistas/Graficas/code/highcharts.js"></script>
 <script src="<?php echo SERVERURL; ?>vistas/Graficas/code/modules/data.js"></script>
@@ -191,62 +198,46 @@ if($_SESSION['privilegio_spm']!=1){
 	<div class="col-md-6">
 
 		<figure class="highcharts-figure">
-		    <div id="container_3D">
-		    	
-		    </div>
-		</figure>
+    <div id="container"></div>
+    <p class="highcharts-description">
+        A variation of a 3D pie chart with an inner radius added.
+        These charts are often referred to as donut charts.
+    </p>
+</figure>
 
 
 		<script type="text/javascript">
-				Highcharts.chart('container_3D', {
-				    chart: {
-				        type: 'column',
-				        options3d: {
-				            enabled: true,
-				            alpha: 10,
-				            beta: 25,
-				            depth: 70
-				        }
-				    },
-				    title: {
-				        text: 'REPORTE DE ACTIVIDADES POR CASOS - 3D'
-				    },
-				    subtitle: {
-				        text: 'Notice the difference between a 0 value and a null point'
-				    },
-				    plotOptions: {
-				        column: {
-				            depth: 25
-				        }
-				    },
-				    xAxis: {
-				        categories: [
-				        <?php  foreach($datos as $row){ 
-				        ?>
-				        	['<?php echo $row['CASnombre']?>'],
-				        <?php } ?>
-				        ],
-				  
-				    },
-				    yAxis: {
-				        title: {
-				            text: null
-				        }
-				    },
-				    series: [{
-				        name: 'Casos',
-				        data: [
-				        <?php  foreach($datos as $row){ 
-				        ?>
-
-				        	[<?php echo $row['Contador']?>],
-
-				        	<?php } ?>
-				        ]
-				         
-				    }]
-				});
-						</script>
+Highcharts.chart('container', {
+    chart: {
+        type: 'pie',
+        options3d: {
+            enabled: true,
+            alpha: 45
+        }
+    },
+    title: {
+        text: 'Contents of Highsoft\'s weekly fruit delivery'
+    },
+    subtitle: {
+        text: '3D donut in Highcharts'
+    },
+    plotOptions: {
+        pie: {
+            innerSize: 100,
+            depth: 45
+        }
+    },
+    series: [{
+        name: 'Delivered amount',
+        data: [
+            <?php  foreach($datos as $row){ 
+	                        	echo " ['".$row['CASnombre']."', ".$row['Contador']."],";
+	                          }  
+	                        ?>
+        ]
+    }]
+});
+		</script>
 	</div>
 
 </div>
@@ -320,77 +311,63 @@ if($_SESSION['privilegio_spm']!=1){
 
 		<div class="col-md-6">
 
-<figure class="highcharts-figure">
-    <div id="container"></div>
-    <p class="highcharts-description">
-        A basic column chart compares rainfall values between four cities.
-        Tokyo has the overall highest amount of rainfall, followed by New York.
-        The chart is making use of the axis crosshair feature, to highlight
-        months as they are hovered over.
-    </p>
-</figure>
-
+			<figure class="highcharts-figure">
+		    <div id="container_3D">
+		    	
+		    </div>
+		</figure>
 
 
 		<script type="text/javascript">
-Highcharts.chart('container', {
-    chart: {
-        type: 'column'
-    },
-    title: {
-        text: 'Monthly Average Rainfall'
-    },
-    subtitle: {
-        text: 'Source: WorldClimate.com'
-    },
-    xAxis: {
-        categories: [
-            <?php  foreach($datos as $row){ 
+				Highcharts.chart('container_3D', {
+				    chart: {
+				        type: 'column',
+				        options3d: {
+				            enabled: true,
+				            alpha: 10,
+				            beta: 25,
+				            depth: 70
+				        }
+				    },
+				    title: {
+				        text: 'REPORTE DE ACTIVIDADES POR CASOS - 3D'
+				    },
+				    subtitle: {
+				        text: 'Notice the difference between a 0 value and a null point'
+				    },
+				    plotOptions: {
+				        column: {
+				            depth: 25
+				        }
+				    },
+				    xAxis: {
+				        categories: [
+				        <?php  foreach($datos as $row){ 
 				        ?>
-				        	['<?php echo $row['TIPnombre']?>'],
+				        	['<?php echo $row['CASnombre']?>'],
 				        <?php } ?>
-        ],
-        crosshair: true
-    },
-    yAxis: {
-        min: 0,
-        title: {
-            text: 'Rainfall (mm)'
-        }
-    },
-    tooltip: {
-        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-            '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
-        footerFormat: '</table>',
-        shared: true,
-        useHTML: true
-    },
-    plotOptions: {
-        column: {
-            pointPadding: 0.2,
-            borderWidth: 0
-        }
-    },
-    series: [{
-        name: 'Tokyo',
-        data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+				        ],
+				  
+				    },
+				    yAxis: {
+				        title: {
+				            text: null
+				        }
+				    },
+				    series: [{
+				        name: 'Casos',
+				        data: [
+				        <?php  foreach($datos as $row){ 
+				        ?>
 
-    }, {
-        name: 'New York',
-        data: [83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0, 104.3, 91.2, 83.5, 106.6, 92.3]
+				        	[<?php echo $row['Contador']?>],
 
-    }, {
-        name: 'London',
-        data: [48.9, 38.8, 39.3, 41.4, 47.0, 48.3, 59.0, 59.6, 52.4, 65.2, 59.3, 51.2]
-
-    }, {
-        name: 'Berlin',
-        data: [42.4, 33.2, 34.5, 39.7, 52.6, 75.5, 57.4, 60.4, 47.6, 39.1, 46.8, 51.1]
-
-    }]
-});
-		</script>
+				        	<?php } ?>
+				        ]
+				         
+				    }]
+				});
+						</script>
 		</div>
 
 
